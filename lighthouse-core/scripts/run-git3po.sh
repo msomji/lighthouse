1d5e806862a14e577fb9c2c66eb3b24c52aa1415
Paul Irish <paulirish@google.com>
2020-01-15 15:07:38 -0800

    add script to run git3po

---
+++ lighthouse-core/scripts/run-git3po.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# requires a ghtoken set as GIT3PO_GH_TOKEN
+# likely invocation:
+#     env GIT3PO_GH_TOKEN=<token> lighthouse-core/scripts/run-git3po.sh
+
+set -ex
+
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# git3po assumes relative config paths from cwd.
+cd "$DIRNAME"
+
+npm install -g git3po
+
+find git3po-rules/*.yaml -exec git3po -c {} \;
